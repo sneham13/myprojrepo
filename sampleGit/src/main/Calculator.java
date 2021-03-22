887943bec5b53f95b9aed564a6c892d69f0ba18d
Sneham13 <snehamurli13@gmail.com>
2021-03-22 18:27:21 +0530

    added new changes

--- sampleGit/src/main/Calculator.java
+++ sampleGit/src/main/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		add(10,20);
 	}
 	public static void add(int a,int b){
 	      System.out.println(a+b);
