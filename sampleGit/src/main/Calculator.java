60d6ed5e31867f68e025a2e3ff1d21d430b10b66
sneham13 <60093792+sneham13@users.noreply.github.com>
2021-03-22 18:30:01 +0530

    added sub func

--- sampleGit/src/main/Calculator.java
+++ sampleGit/src/main/Calculator.java
@@ -10,5 +10,8 @@ public class Calculator {
 	public static void add(int a,int b){
 	      System.out.println(a+b);
 	  }
+	public static void sub(int a,int b){
+	      System.out.println(a-b);
+	  }
 
 }
